321e5d185aa5c252b284c4ddc29b9ea4a11afe77
DawoodSaeed <dawoodsaeed18@gmail.com>
2024-01-15 23:39:06 +0500

    profile overview

--- scripts/common.js
+++ scripts/common.js
@@ -15,3 +15,63 @@ function getCountries(lang = "en") {
   }
   return countries;
 }
+
+// Set the privacy statement
+function setPrivacyStatement() {
+  const privacyStatement = document.getElementById("privacyStatement");
+  //   helper method for the class toggling
+  const toggleClass = function (elem, className) {
+    if (elem.classList.contains(className)) {
+      elem.classList.remove(className);
+    } else {
+      elem.classList.add(className);
+    }
+  };
+  // For the privacy checkbox;
+  const privacyCheckBoxLabel = document.getElementById("privacyCheckboxLabel");
+  const privacyCheckBox = document.getElementById("privacyCheckBox");
+
+  //   check if the checkbox is already checked;
+
+  if (localStorage.getItem("privacy") && localStorage.getItem("time")) {
+    privacyCheckBox.checked = true;
+    privacyStatement.textContent = `You agreed to our privacy policy on ${localStorage.getItem(
+      "time"
+    )}`;
+    toggleClass(privacyCheckBoxLabel, "active");
+    console.log("Privacy already set");
+  }
+
+  // For changing the background of the label
+  privacyCheckBoxLabel.addEventListener("click", function () {
+    toggleClass(this, "active");
+    // Check the state of the checkbox;
+  });
+
+  // TGo detect the chnage in orignal checkbox; and rhe store in it label;
+  privacyCheckBox.addEventListener("change", function () {
+    if (this.checked) {
+      const currentDate = new Date();
+
+      // Format the date as "dd.mm.yyyy"
+      const formattedDate = `${padNumber(currentDate.getDate())}.${padNumber(
+        currentDate.getMonth() + 1
+      )}.${currentDate.getFullYear()}.`;
+
+      // Display or use the formatted date as needed
+      console.log(formattedDate);
+      localStorage.setItem("privacy", true);
+      localStorage.setItem("time", formattedDate);
+      console.log("Saved in the Storage");
+    } else {
+      localStorage.removeItem("privacy");
+      localStorage.removeItem("time");
+      console.log("Removed from the Storage");
+    }
+  });
+
+  // Function to pad a number with leading zero if needed
+  function padNumber(num) {
+    return num.toString().padStart(2, "0");
+  }
+}
